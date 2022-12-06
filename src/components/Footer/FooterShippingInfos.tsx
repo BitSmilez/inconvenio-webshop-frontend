4ff77741f85d10020470ddf21682527825bae01d
jutnh <jutnhbr@gmail.com>
2022-12-07 00:47:09 +0100

    [+] Footer with Shop Details, Relevant Links, Payment and Shipping Information

---
+++ src/components/Footer/FooterShippingInfos.tsx
@@ -0,0 +1,15 @@
+import "./Footer.css"
+const FooterShippingInfos = () => {
+
+    return (
+        <>
+            <h2 className={"footer-headlines"}>SHIPPING INFORMATION/</h2>
+            <div className={"footer-text"}>
+                <p> FREE SHIPPING INSIDE GERMANY<br></br>
+                    10€ SHIPPING FEE FOR OTHER COUNTRIES</p>
+            </div>
+        </>
+    )
+}
+
+export default FooterShippingInfos;
