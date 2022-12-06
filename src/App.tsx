1e5ac90baad305ef4e7b2f44136d2faff8441b27
jutnh <jutnhbr@gmail.com>
2022-12-06 18:37:28 +0100

    [+] Added AdBanner to all Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import LandingPage from "./pages/LandingPage";
+import AdBanner from "./components/AdBanner/AdBanner";
 
 const App = () => {
-    return (
 
-        <Router>
-            <Routes>
-                <Route path="/" element={<LandingPage/>}/>
-            </Routes>
-        </Router>
-    )
+    // TODO: Dynamic Messages from API
+    const bannerMessage = "FREE SHIPPING ON ORDERS OVER $100";
+
+
+    return (
+        <>
+            {/* Components that are always shown on every Route */}
+            <AdBanner bannerMessage={bannerMessage}/>
+            {/* Routes */}
+            <Router>
+                <Routes>
+                    <Route path="/" element={<LandingPage/>}/>
+                </Routes>
+            </Router>
+        </>
+    );
 }
 
 export default App;
