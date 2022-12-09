7001d161ba4ad1d8f2ef84f05a2300602c0df3f3
jutnh <jutnhbr@gmail.com>
2022-12-09 17:35:53 +0100

    [+] Defined global Shop Constants

---
+++ src/data/shopConstants.js
@@ -0,0 +1,26 @@
+// Global Shop Related Variables / Constants
+const shopName_ = "INCONVENIO";
+const shopGlobalEmail_ = "shop@inconvenio-shop.de";
+const shopSupportEmail_ = "support@inconvenio-shop.de";
+const shopLegalEmail_ = "legalteam@inconvenio-shop.de";
+const shopPhone_ = "+(123) 456-7890";
+const shopAddress_ = "12345 Inco Street, Incocity";
+const shopStreet_ = "Inco Street";
+const shopCity_ = "Incocity";
+const shopCountry_ = "Germany";
+const shopZip_ = "12345";
+const shopURL_ = "https://inconvenio-shop.de/";
+
+export default {
+    shopName: shopName_,
+    shopEmail: shopGlobalEmail_,
+    shopSupportEmail: shopSupportEmail_,
+    shopLegalEmail: shopLegalEmail_,
+    shopPhone: shopPhone_,
+    shopAddress: shopAddress_,
+    shopStreet: shopStreet_,
+    shopCity: shopCity_,
+    shopCountry: shopCountry_,
+    shopZip: shopZip_,
+    shopURL: shopURL_
+}
