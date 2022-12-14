fdbe8b61d67bb4bdbaf9eac163b68eabdaecf385
jutnh <jutnhbr@gmail.com>
2022-12-15 00:15:18 +0100

    [+] Added ProductList to LandingPage

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,10 +1,12 @@
 import NavBar from "../components/NavBar/NavBar";
 import Footer from "../components/Footer/Footer";
+import ProductList from "../components/ProductList/ProductList";
 
 const LandingPage = () => {
     return (
         <>
             <NavBar/>
+            <ProductList url={"http://localhost:8080/products"}/>
             <Footer/>
         </>
     );
