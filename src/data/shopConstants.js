a9d01872bd04b689e650a883452d6b487c473d0c
jutnh <jutnhbr@gmail.com>
2022-12-14 18:30:11 +0100

    [/] Fixed CI Warning: "Assign object to a variable before exporting as module default"

--- src/data/shopConstants.js
+++ src/data/shopConstants.js
@@ -1,26 +1,16 @@
 // Global Shop Related Variables / Constants
-const shopName_ = "INCONVENIO";
-const shopGlobalEmail_ = "shop@inconvenio-shop.de";
-const shopSupportEmail_ = "support@inconvenio-shop.de";
-const shopLegalEmail_ = "legalteam@inconvenio-shop.de";
-const shopPhone_ = "+(123) 456-7890";
-const shopAddress_ = "12345 Inco Street, Incocity";
-const shopStreet_ = "Inco Street";
-const shopCity_ = "Incocity";
-const shopCountry_ = "Germany";
-const shopZip_ = "12345";
-const shopURL_ = "https://inconvenio-shop.de/";
-
-export default {
-    shopName: shopName_,
-    shopEmail: shopGlobalEmail_,
-    shopSupportEmail: shopSupportEmail_,
-    shopLegalEmail: shopLegalEmail_,
-    shopPhone: shopPhone_,
-    shopAddress: shopAddress_,
-    shopStreet: shopStreet_,
-    shopCity: shopCity_,
-    shopCountry: shopCountry_,
-    shopZip: shopZip_,
-    shopURL: shopURL_
+const shopConstants = {
+    shopName: "INCONVENIO",
+    shopEmail: "shop@inconvenio-shop.de",
+    shopSupportEmail: "support@inconvenio-shop.de",
+    shopLegalEmail: "legalteam@inconvenio-shop.de",
+    shopPhone: "+(123) 456-7890",
+    shopAddress: "12345 Inco Street, Incocity",
+    shopStreet: "Inco Street",
+    shopCity: "Incocity",
+    shopCountry: "Germany",
+    shopZip: "12345",
+    shopURL: "https://inconvenio-shop.de/"
 }
+
+export default shopConstants;
