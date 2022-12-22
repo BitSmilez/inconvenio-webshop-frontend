5a88e0c9585c5be94e738e1e7d3e0c620a2fca0f
jutnh <jutnhbr@gmail.com>
2022-12-23 00:38:33 +0100

    INCONV-7 [/] Updated Tests (Now uses MemoryRouter since the component uses navigator)

--- src/components/ProductItem/ProductItem.cy.tsx
+++ src/components/ProductItem/ProductItem.cy.tsx
@@ -1,4 +1,5 @@
 import ProductItem from "./ProductItem";
+import {MemoryRouter as TestRouter} from 'react-router-dom';
 
 const product_normal = {
     name: "The Product",
@@ -19,67 +20,123 @@ const discount = Math.floor((product_with_sales_price.price - product_with_sales
 
 describe("Normal ProductItem", () => {
     it("should mount", () => {
-        cy.mount(<ProductItem product={product_normal}/>)
+        cy.mount(
+            <TestRouter>
+                <ProductItem product={product_normal}/>
+            </TestRouter>
+        )
     });
     it("should have the correct name", () => {
-        cy.mount(<ProductItem product={product_normal}/>)
+        cy.mount(
+            <TestRouter>
+                <ProductItem product={product_normal}/>
+            </TestRouter>
+        )
         cy.get('.product-item-name').contains("The Product")
     });
     it("should have the correct description", () => {
-        cy.mount(<ProductItem product={product_normal}/>)
+        cy.mount(
+            <TestRouter>
+                <ProductItem product={product_normal}/>
+            </TestRouter>
+        )
         cy.get('.product-item-desc').contains("Product 1 fancy description")
     });
     it("should have an image area (does not have to load)", () => {
-        cy.mount(<ProductItem product={product_normal}/>)
+        cy.mount(
+            <TestRouter>
+                <ProductItem product={product_normal}/>
+            </TestRouter>
+        )
         cy.get('.product-image').should("be.visible")
 
     });
     it("should have the correct class for the price", () => {
-        cy.mount(<ProductItem product={product_normal}/>)
+        cy.mount(
+            <TestRouter>
+                <ProductItem product={product_normal}/>
+            </TestRouter>
+        )
         cy.get('.product-item-price-normal').should('exist')
     });
     it("should have the correct price", () => {
-        cy.mount(<ProductItem product={product_normal}/>)
+        cy.mount(
+            <TestRouter>
+                <ProductItem product={product_normal}/>
+            </TestRouter>
+        )
         cy.get('.product-item-price-normal').contains("200.00")
     });
     it("should not have a sales price", () => {
-        cy.mount(<ProductItem product={product_normal}/>)
+        cy.mount(
+            <TestRouter>
+                <ProductItem product={product_normal}/>
+            </TestRouter>
+        )
         cy.get('.product-item-sales-price').should('not.exist')
     });
 });
 
 describe("ProductItem with sales price", () => {
     it("should mount", () => {
-        cy.mount(<ProductItem product={product_with_sales_price}/>)
+        cy.mount(
+            <TestRouter>
+                <ProductItem product={product_with_sales_price}/>
+            </TestRouter>
+        )
     });
     it("should have the correct class for the price", () => {
-        cy.mount(<ProductItem product={product_with_sales_price}/>)
+        cy.mount(
+            <TestRouter>
+                <ProductItem product={product_with_sales_price}/>
+            </TestRouter>
+        )
         cy.get('.product-item-price-if-on-sale').should('exist')
     });
     it("should have the correct sales price in green color", () => {
-        cy.mount(<ProductItem product={product_with_sales_price}/>)
+        cy.mount(
+            <TestRouter>
+                <ProductItem product={product_with_sales_price}/>
+            </TestRouter>
+        )
         cy.get('.product-item-sales-price')
             .contains("100.00")
             .and('have.css', 'color', 'rgb(0, 128, 0)')
 
     });
     it("should display the old price correctly and in red color", () => {
-        cy.mount(<ProductItem product={product_with_sales_price}/>)
+        cy.mount(
+            <TestRouter>
+                <ProductItem product={product_with_sales_price}/>
+            </TestRouter>
+        )
         cy.get('.product-item-price-if-on-sale')
             .should('exist')
             .and('have.css', 'color', 'rgb(255, 0, 0)')
             .and("contain", "200.00")
     });
     it("should have the old price crossed out", () => {
-        cy.mount(<ProductItem product={product_with_sales_price}/>)
+        cy.mount(
+            <TestRouter>
+                <ProductItem product={product_with_sales_price}/>
+            </TestRouter>
+        )
         cy.get('.product-item-price-if-on-sale').should('have.css', 'text-decoration-line', 'line-through')
     });
     it("should have a discount badge", () => {
-        cy.mount(<ProductItem product={product_with_sales_price}/>)
+        cy.mount(
+            <TestRouter>
+                <ProductItem product={product_with_sales_price}/>
+            </TestRouter>
+        )
         cy.get('.discount-badge').should('exist')
     });
     it("should have the correct discount percentage in the badge", () => {
-        cy.mount(<ProductItem product={product_with_sales_price}/>)
+        cy.mount(
+            <TestRouter>
+                <ProductItem product={product_with_sales_price}/>
+            </TestRouter>
+        )
         cy.get('.discount-badge').contains(discount)
     });
 });
