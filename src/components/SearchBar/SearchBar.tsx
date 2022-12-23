bf5f939f7b93bfcfc89ed813ebfe4997d976d594
Biri <test@email>
2022-12-23 17:23:57 +0100

    INCONV-33 [+] on enter trigger search

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -8,15 +8,20 @@ const SearchBar = () => {
     const handleSearch = () => {
         navigate(`/products/search/${searchQuery}`);
     };
+    const handleKeyDown = (event: any) => {
+        if (event.key === 'Enter') {
+            handleSearch();
+        }
+    }
+        return (
+            <div className="search-bar">
+                <input type="text" onKeyDown={handleKeyDown} value={searchQuery}
+                       onChange={e => setSearchQuery(e.target.value)}/>&nbsp;
+                <button onClick={handleSearch}>Search</button>
+            </div>
+        );
 
-    return (
-        <div className="search-bar">
-            <input type="text" value={searchQuery} onChange={e => setSearchQuery(e.target.value)} />&nbsp;
-             <button onClick={handleSearch}>Search</button>
-        </div>
-    );
+
 }
-
-
 
 export default SearchBar;
