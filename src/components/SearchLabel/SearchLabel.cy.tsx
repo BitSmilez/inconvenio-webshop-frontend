df539bdd48d6bb2e42986575b4e3482e87b18f9c
jutnh <jutnhbr@gmail.com>
2022-12-24 20:36:39 +0100

    INCONV-33 [/] SearchLabel shows the SearchQuery, Category or "All Products" on homepage - Cypress Tests

---
+++ src/components/SearchLabel/SearchLabel.cy.tsx
@@ -0,0 +1,11 @@
+import SearchLabel from "./SearchLabel";
+
+describe("SearchLabel", () => {
+    it("should mount", () => {
+        cy.mount(<SearchLabel text={"Test Label"}/>);
+    });
+    it("should have the correct label", () => {
+        cy.mount(<SearchLabel text={"Test Label"}/>);
+        cy.get(".search-label").contains("Test Label");
+    });
+});
