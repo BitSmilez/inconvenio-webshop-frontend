409105fa2bf04d647630e0d58fb531db0b688cf3
jutnh <jutnhbr@gmail.com>
2022-12-24 17:53:45 +0100

    INCONV-33 [+] ProductList now takes a title prop and displays an error div when no products are found - Adjusted CyPress Tests

--- src/components/ProductList/ProductList.cy.tsx
+++ src/components/ProductList/ProductList.cy.tsx
@@ -2,10 +2,10 @@ import ProductList from "./ProductList";
 
 describe("ProductList", () => {
     it("should mount", () => {
-        cy.mount(<ProductList url={"/"}/>)
+        cy.mount(<ProductList label={"Products"} url={"/"}/>)
     });
     it("should throw an error if there is no valid data (like in this case)", () => {
-        cy.mount(<ProductList url={"/"}/>)
+        cy.mount(<ProductList label={"Products"} url={"/"}/>)
         cy.get('.product-container > div').contains("Unexpected token '<', \"<!DOCTYPE \"... is not valid JSON")
     });
 });
