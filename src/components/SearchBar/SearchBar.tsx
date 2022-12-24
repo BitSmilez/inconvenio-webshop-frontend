b96fd03ba074e539f5161605dee838dfc49c7640
jutnh <jutnhbr@gmail.com>
2022-12-24 20:37:16 +0100

    INCONV-33 [+] Input Validation and dynamic CSS Class on error - Cypress Tests

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -5,8 +5,16 @@ import { useNavigate} from 'react-router-dom';
 const SearchBar = () => {
     const [searchQuery, setSearchQuery] = React.useState('');
     const navigate = useNavigate()
+    const [errorState, setErrorState] = React.useState(false);
+
     const handleSearch = () => {
-        navigate(`/products/search/${searchQuery}`);
+        if(searchQuery.length > 0 && searchQuery.replace(/\s/g, "").length) {
+            setErrorState(false);
+            navigate(`/products/search/${searchQuery.trim().replace(/[^a-z0-9\s]/gi, '')}`);
+        }
+        else {
+            setErrorState(true);
+        }
     };
     const handleKeyDown = (event: any) => {
         if (event.key === 'Enter') {
@@ -15,7 +23,7 @@ const SearchBar = () => {
     }
         return (
             <div className="search-bar">
-                <input type="text" onKeyDown={handleKeyDown} value={searchQuery}
+                <input className={!errorState ? "search-bar-input" : "search-bar-input-on-error"} type="text" onKeyDown={handleKeyDown} value={searchQuery} placeholder={"Search INCONVENIO..."}
                        onChange={e => setSearchQuery(e.target.value)}/>&nbsp;
                 <button onClick={handleSearch}>Search</button>
             </div>
