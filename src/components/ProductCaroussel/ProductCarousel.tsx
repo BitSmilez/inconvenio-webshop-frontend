2b8874287d9471801920252868f73e1b86e42b96
jutnh <jutnhbr@gmail.com>
2022-12-26 20:40:09 +0100

    INCONV-5 [+] Centered Carousel, added margin, changed font

--- src/components/ProductCaroussel/ProductCarousel.tsx
+++ src/components/ProductCaroussel/ProductCarousel.tsx
@@ -1,22 +1,24 @@
 import {useFetch} from "../../hooks/useFetch";
 import ProductItem from "../ProductItem/ProductItem";
 import "./ProductCarousel.css";
-const ProductCarousel = ({category,remove_id}:{category:string,remove_id:string}) => {
+
+const ProductCarousel = ({category, remove_id}: { category: string, remove_id: string }) => {
 
     let url = `http://localhost:8080/product/category/${category}`;
     const {fetchedData: fetched_products} = useFetch(url);
-   let products = fetched_products.filter((product:any) => product.id !== remove_id);
+    let products = fetched_products.filter((product: any) => product.id !== remove_id);
+
     return (
-        <div>
-        <h1>You might like these totally useful awesomeness products!</h1>
-        <div className={"caroussel"}>
-            {products && products.map((product: any) => (
-                <ProductItem key={product.id} product={product}/>
-            ))}
-        </div>
+        <div className={"caroussel-wrapper"}>
+            <h1>You might like these totally useful awesomeness products!</h1>
+            <div className={"caroussel"}>
+                {products && products.map((product: any) => (
+                    <ProductItem key={product.id} product={product}/>
+                ))}
+            </div>
         </div>
 
-            );
+    );
 
 }
 
