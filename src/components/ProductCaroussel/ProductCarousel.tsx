75c5783a059ea7a1c1bade43b4498531c26318fc
Biri <test@email>
2022-12-26 18:52:51 +0100

    INCONV-5 [+] added param to remove current product from slider

--- src/components/ProductCaroussel/ProductCarousel.tsx
+++ src/components/ProductCaroussel/ProductCarousel.tsx
@@ -1,11 +1,11 @@
 import {useFetch} from "../../hooks/useFetch";
 import ProductItem from "../ProductItem/ProductItem";
 import "./ProductCarousel.css";
-const ProductCarousel = ({category}:{category:string}) => {
+const ProductCarousel = ({category,remove_id}:{category:string,remove_id:string}) => {
 
     let url = `http://localhost:8080/product/category/${category}`;
-    const {fetchedData: products} = useFetch(url);
-
+    const {fetchedData: fetched_products} = useFetch(url);
+   let products = fetched_products.filter((product:any) => product.id !== remove_id);
     return (
         <div>
         <h1>You might like these totally useful awesomeness products!</h1>
