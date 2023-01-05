c3591c52bc1ac2f2d65deadfe21510fe80c135a7
Biri <test@email>
2023-01-05 15:44:49 +0100

    INCONV-21 [+] added Cart page

---
+++ src/pages/CartPage.tsx
@@ -0,0 +1,13 @@
+import CartContent from "../components/CartContent/CartContent";
+
+const CartPage = () => {
+    return (
+        <div>
+            <h1>Cart Page</h1>
+            <CartContent/>
+        </div>
+    );
+
+};
+
+export default CartPage;
