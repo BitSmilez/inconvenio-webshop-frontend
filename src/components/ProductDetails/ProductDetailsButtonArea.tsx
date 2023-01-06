acedb7e0cb59edbb3f39446043a8f689e88261d7
jutnh <jutnhbr@gmail.com>
2023-01-06 23:56:18 +0100

    INCONV-10 [+] Added addToCart onClick Listener

--- src/components/ProductDetails/ProductDetailsButtonArea.tsx
+++ src/components/ProductDetails/ProductDetailsButtonArea.tsx
@@ -2,10 +2,11 @@ import "./ProductDetails.css"
 import {Button, IconButton} from "@mui/material";
 import {AddShoppingCart, FavoriteBorder} from "@mui/icons-material";
 
-const ProductDetailsButtonArea = () => {
-    return(
+const ProductDetailsButtonArea = ({addToCart}: { addToCart: any }) => {
+
+    return (
         <div className={"product-details-button-area"}>
-            <Button sx={{
+            <Button onClick={addToCart} sx={{
                 borderRadius: 10
             }} size="large" variant="outlined"> <AddShoppingCart/>ADD TO CART</Button>
             <IconButton size="large" color="primary" aria-label="add to shopping cart">
