98c0bb741c117e2fb30b3e4aac2d2267d80814b4
jutnh <jutnhbr@gmail.com>
2023-01-07 22:42:49 +0100

    INCONV-21 [+] Rewritten CartItem, fixed price formatting, improved layout

--- src/components/CartProductItem/CartProductItem.tsx
+++ src/components/CartProductItem/CartProductItem.tsx
@@ -1,45 +1,46 @@
 import {IconButton} from "@mui/material";
 import {Add, Remove} from "@mui/icons-material";
 import "./CartProductItem.css";
-const CartProductItem =({product}:{product:any}) => {
 
+const CartProductItem = ({product}: { product: any }) => {
 
     let price = product.productSalesPrice ? product.productSalesPrice : product.productPrice;
     let name = product.productName
     let img = product.productImg
     let quantity = product.quantity
     let total = price * quantity
-    console.log(product)
+
     return (
         <>
-
-
-            <div className={"products-cart"}>
-                <div className={"products-cart-container"}>
-                    <div className={"products-cart-item"}>
-                        <img className={"cart-prod-img"} src={img} alt="prodImg"></img>
-                        <div className={"products-cart-item-details"}>
-                            <h3> {name} </h3>
-                            <div className={"cart-item-quantity"}>
-                                <IconButton className={"cart-item-btn"} color="default" aria-label="Add to cart">
-                                    <Add/>
-                                </IconButton>
-                                <p> {quantity}</p>
-                                <IconButton className={"cart-item-btn"} color="default" aria-label="Add to cart">
-                                    <Remove/>
-                                </IconButton>
-                            </div>
-                            <p> Total:{total}€ </p>
+            <div className={"cart-product-info"}>
+                <div className={"cart-product-container"}>
+                    <div className={"cart-product-details"}>
+                        <img src={img} alt={name}/>
+                        <div className={"cart-product-general"}>
+                            <h2>{name}</h2>
+                            <p>Item: ${price.toFixed(2)}</p>
                         </div>
-                        <div className={"products-cart-item-actions"}>
-
+                        <div className={"cart-product-price-info"}>
+                            <div className={"cart-product-quantity-selector"}>
+                                <p className={"cart-product-quantity-label"}>Quantity</p>
+                                <div className={"cart-product-amount"}>
+                                    <IconButton color="default" aria-label="Add to cart">
+                                        <Add/>
+                                    </IconButton>
+                                    <p> {quantity}</p>
+                                    <IconButton color="default" aria-label="Add to cart">
+                                        <Remove/>
+                                    </IconButton>
+                                </div>
+                            </div>
+                            <div className={"cart-product-total"}>
+                                <p> Total: ${total.toFixed(2)} </p>
+                            </div>
                         </div>
                     </div>
                 </div>
-
             </div>
         </>
-
     )
 }
 export default CartProductItem;
