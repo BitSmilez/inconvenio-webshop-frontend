cb834b46cfc996d69353d3be6076ef9c8fe283c6
jutnh <jutnhbr@gmail.com>
2023-01-07 22:37:26 +0100

    INCONV-21 [+] ProductList now has a dynamic css class and a callback function to return data to parent

--- src/components/ProductList/ProductList.tsx
+++ src/components/ProductList/ProductList.tsx
@@ -4,22 +4,33 @@ import CircularLoader from "../CircularLoader/CircularLoader";
 import "./ProductList.css";
 import SearchLabel from "../SearchLabel/SearchLabel";
 import NotifyDiv from "../NotifyDiv/NotifyDiv";
+import {useEffect} from "react";
+import CartProductItem from "../CartProductItem/CartProductItem";
 
-const ProductList = ({url, label}: { url: string, label: string }) => {
+const ProductList = ({url, label, callback}: { url: string, label: string, callback: any }) => {
 
-    const {fetchedData: products, isLoading, errMsg} = useFetch(url);
+    const {fetchedData: products, isLoading, errMsg}: any = useFetch(url);
 
+    const handleCallback = (products: any) => {
+        callback(products);
+    }
+
+    useEffect(() => {
+        if (callback) {
+            handleCallback(products);
+        }
+    }, [products])
 
     return (
         <>
             {isLoading && <CircularLoader/>}
             {label && <SearchLabel text={label}/>}
-            <div className={"product-container"}>
+            <div className={callback == null ? "product-container" : "cart-container"}>
                 {errMsg && <div> {errMsg} </div>}
                 {products.length === 0 && <NotifyDiv notifyMessage={"Sorry, no products found :("}/>}
-                {products && products.map((product: any) => (
-                    <ProductItem key={product.id} product={product}/>
-                ))}
+                {products && products.items == null
+                    ? products.map((product: any) => (<ProductItem key={product.id} product={product}/>))
+                    : products.items.map((product: any) => (<CartProductItem key={product.productID} product={product}/>))}
             </div>
         </>
     )
