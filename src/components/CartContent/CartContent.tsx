57bd76bd4604d5ca510aeda79bc631cf79b0a27c
jutnh <jutnhbr@gmail.com>
2023-01-07 22:41:34 +0100

    INCONV-21 [+] Rewritten CartContent -> Now reuses ProductList to display CartItems [+] Uses CartSummaryDetails [+] Uses ProductList Callback to pass down fetched product data to its children [+] Added PageHeadLine [+] Tests

--- src/components/CartContent/CartContent.tsx
+++ src/components/CartContent/CartContent.tsx
@@ -1,47 +1,35 @@
-import CartProductItem from "../CartProductItem/CartProductItem";
 import "./CartContent.css"
 import {ShoppingBag} from "@mui/icons-material";
-import {useFetch} from "../../hooks/useFetch";
 import LinkButton from "../LinkButton/LinkButton";
+import ProductList from "../ProductList/ProductList";
+import {useState} from "react";
+import CartSummaryDetails from "../CartSummaryDetails/CartSummaryDetails";
+import GenericPageHeadline from "../GenericPageHeadline/GenericPageHeadline";
+
 const CartContent = () => {
 
-    const CustomDivider = ({ color }: {color: string}) => {
-        return (
-            <hr  style={{
-                color: color,
-                backgroundColor: color,
-                height: .5,
-            }}/>
-        )
-    }
+    const [cart, setCart] = useState<any>([]);
 
-    const {fetchedData: cart}: any =  useFetch("http://localhost:8081/cart/1234")
-    console.log(cart)
-    let products = cart.items
     return (
-        <>
+        <div className={"cart-wrapper"}>
+            <div className={"cart-page-headline"}>
+                <GenericPageHeadline text={"YOUR CART"}/>
+            </div>
             <div className={"cart-button-row"}>
                 <LinkButton URL={"/"} text={"Continue Shopping"}></LinkButton>
                 <LinkButton URL={"/checkout"} text={"Checkout"}></LinkButton>
             </div>
-            {products && products.map((product: any) => (
-                <CartProductItem key={product.productID} product={product}/>
-            ))}
+            <div className={"cart-content-wrapper"}>
+                <ProductList url={"http://localhost:8081/cart/1234"} label={""} callback={setCart}/>
                 <div className={"cart-details-container"}>
                     <div className={"cart-summary-headline-container"}>
                         <h1 className={"cart-summary-headline"}>YOUR ORDER IS READY</h1>
                         <ShoppingBag fontSize={"large"} sx={{mt: 1.75}}/>
                     </div>
-                    <div className={"cart-summary-details"}>
-                        <CustomDivider color={"#000000"}/>
-                        <p> Price: {cart.subTotal}€ <br/> including {cart.subTotalVat}€ VAT ({cart.taxRate}%)  </p>
-                        <p> Shipping: {cart.shipping}€ </p>
-                        <CustomDivider color={"#000000"}/>
-                        <p className={"cart-summary-total"}> Total: {cart.total}€ <br/> including {cart.totalVat}€ VAT ({cart.taxRate}%)  </p>
-                    </div>
+                    <CartSummaryDetails cart={cart}/>
                 </div>
-
-        </>
+            </div>
+        </div>
     )
 }
 export default CartContent;
