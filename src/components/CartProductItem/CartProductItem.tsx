b6b5856e767d1befbf525036b55e3ef15c69f3f6
jutnh <jutnhbr@gmail.com>
2023-01-13 22:40:41 +0100

    INCONV-20 [+] Added handleRemoveFromCart

--- src/components/CartProductItem/CartProductItem.tsx
+++ src/components/CartProductItem/CartProductItem.tsx
@@ -1,17 +1,22 @@
 import {IconButton} from "@mui/material";
-import {Add, Remove} from "@mui/icons-material";
+import {Add, HighlightOff, Remove} from "@mui/icons-material";
 import "./CartProductItem.css";
 import {useNavigate} from "react-router-dom";
+import {useContext} from "react";
+import {CustomerContext} from "../../context/CustomerContext";
+import {useAlert} from 'react-alert'
+
 
 const CartProductItem = ({product}: { product: any }) => {
 
     const navigate = useNavigate();
+    const alert = useAlert()
 
     let price = product.productSalesPrice ? product.productSalesPrice : product.productPrice;
-    let name = product.productName
-    let img = product.productImg
     let quantity = product.quantity
     let total = price * quantity
+
+    const {customer}: any = useContext(CustomerContext);
 
     const handleGoBackToDetails = () => {
         navigate({
@@ -20,14 +25,42 @@ const CartProductItem = ({product}: { product: any }) => {
         });
     }
 
+    const handleRemoveFromCart = async (e: any) => {
+        e.stopPropagation();
+        fetch("http://localhost:8080/remove-from-cart", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                productID: product.productID,
+                cartID: customer.customerID,
+            }),
+        })
+            .then((res) => {
+                if (res.status === 200) {
+                    window.location.reload();
+                } else {
+                    alert.show("Something went wrong :/")
+                }
+            });
+    }
+
+
     return (
         <>
             <div onClick={handleGoBackToDetails} className={"cart-product-info"}>
                 <div className={"cart-product-container"}>
                     <div className={"cart-product-details"}>
-                        <img src={img} alt={name}/>
+                        <IconButton onClick={(e) => handleRemoveFromCart(e)}
+                                    sx={{position: "absolute"}}
+                                    className={"pList-add-to-cart-icon"} color="default"
+                                    aria-label="Add to cart">
+                            <HighlightOff/>
+                        </IconButton>
+                        <img src={product.productImg} alt={product.productName}/>
                         <div className={"cart-product-general"}>
-                            <h2>{name}</h2>
+                            <h2>{product.productName}</h2>
                             <p>Item: ${price.toFixed(2)}</p>
                         </div>
                         <div className={"cart-product-price-info"}>
