64420eff0c5b6471caff2973044177315d8bcce9
jutnh <jutnhbr@gmail.com>
2023-01-13 22:40:55 +0100

    INCONV-20 [+] Improved Naming

--- src/context/CustomerContext.tsx
+++ src/context/CustomerContext.tsx
@@ -10,41 +10,40 @@ type CustomerContextProviderProps = {
 
 const CustomerContextProvider = ({children}: CustomerContextProviderProps) => {
     // TOOD: Use customer id for fetching as soon as we have a login
-    const [customerData, setCustomerData] = useState({
+    const [customer, setCustomer] = useState({
         wishlistItemCount: 0,
         cartItemCount: 0,
         isLoggedIn: false,
-        customer: 1,
+        customerID: 1,
     });
-    const {fetchedData: cartCount, isLoading}: any = useFetch("http://localhost:8081/cart/" + customerData.customer)
+    const {fetchedData: cartCount, isLoading}: any = useFetch("http://localhost:8081/cart/" + customer.customerID)
 
 
     useEffect(() => {
         if(!isLoading) {
-            // iterate over cart items and sum up quantity
             let cartItemCount = 0;
             cartCount.items?.forEach((item: any) => {
                 cartItemCount += item.quantity;
             })
-            setCustomerData(customerData => ({...customerData, cartItemCount: cartItemCount}));
+            setCustomer(customer => ({...customer, cartItemCount: cartItemCount}));
         }
     }, [cartCount.items, isLoading])
 
     const changeWishlistItemCount = (count: number) => {
-        setCustomerData({...customerData, wishlistItemCount: customerData.wishlistItemCount + count})
+        setCustomer({...customer, wishlistItemCount: customer.wishlistItemCount + count})
     }
 
     const changeCartItemCount = (count: number) => {
-        setCustomerData({...customerData, cartItemCount: customerData.cartItemCount + count})
+        setCustomer({...customer, cartItemCount: customer.cartItemCount + count})
     }
     const setCartItemCount = (count: number) => {
-        setCustomerData({...customerData, cartItemCount: count})
+        setCustomer({...customer, cartItemCount: count})
     }
 
 
     return (
         <CustomerContext.Provider value={{
-            customerData,
+            customer,
             changeWishlistItemCount,
             changeCartItemCount,
             setCartItemCount
