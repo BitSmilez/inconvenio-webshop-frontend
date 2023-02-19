7c8968c146afaf0104be69bcc0353eb566181199
jutnh <jutnhbr@gmail.com>
2023-02-19 01:50:25 +0100

    INCONV-42 [+] Added CheckoutPage

---
+++ src/pages/CheckoutPage.tsx
@@ -0,0 +1,13 @@
+import NavBar from "../components/NavBar/NavBar";
+import CheckoutContent from "../components/CheckoutContent/CheckoutContent";
+
+const CheckoutPage = () => {
+    return (
+        <>
+            <NavBar/>
+            <CheckoutContent/>
+        </>
+    )
+}
+
+export default CheckoutPage;
